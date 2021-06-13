455bc708323ef67c6aac1c1112790fd85b217c1c
jawaedan20 <m.jawahiruzzaman@gmail.com>
2021-06-13 15:17:33 +0700

    xc

--- components/detail/PanelImage.js
+++ components/detail/PanelImage.js
@@ -2,32 +2,34 @@ import style from "../../styles/detail.module.css";
 import Image from "next/image";
 import { baseUrl } from "../../utils/url";
 import { useState } from "react";
-const PanelImage = ({ data , active}) => {
-  const [key, setKey] = useState(0);
-  return (
-    <div className={style.Image}>
-      <Image
-        className={style.img}
-        src={baseUrl(data.image[key].image)}
-        width="fit-content"
-        height="400px"
-        layout={'responsive'}
-        quality={50}
-      />
-      <div className={style.navImg}>
-        {data.image.map((img, i) => {
-          return (
-            <img
-              className={style.kind}
-              style={i === key ? active : null}
-              src={baseUrl(img.image)}
-              onClick={() => setKey(i)}
+const PanelImage = ({ data, active }) => {
+    const [key, setKey] = useState(0);
+    return (
+        <div className={style.Image}>
+            <Image
+                className={style.img}
+                src="/sapatu.jpg"
+                // src={baseUrl(data.image[key].image)}
+                width="100%"
+                height="fit-content"
+                layout="responsive"
+                quality={50}
             />
-          );
-        })}
-      </div>
-    </div>
-  );
+            <div className={style.navImg}>
+                {data.image.map((img, i) => {
+                    return (
+                        <img
+                            className={style.kind}
+                            style={i === key ? active : null}
+                            src={baseUrl(img.image)}
+                            onClick={() => setKey(i)}
+                            key={i}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default PanelImage;
