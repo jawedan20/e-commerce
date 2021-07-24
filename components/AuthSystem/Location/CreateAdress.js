9fd46c9e0c19ba218a239b143e4c4e75dafaddc4
jawaedan20 <m.jawahiruzzaman@gmail.com>
2021-07-24 12:30:32 +0700

    pretty

--- components/AuthSystem/Location/CreateAdress.js
+++ components/AuthSystem/Location/CreateAdress.js
@@ -9,179 +9,184 @@ import style from "../../../styles/CreateAddress.module.css";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { sendAlert } from "../../../actions/AlertActions";
-import { InputLabel, MenuItem, Select } from "@material-ui/core";
+import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
 import AutoAddress from "./createAdress/AutoAddress";
 import AutoPostal from "./createAdress/AutoPostCode";
 
 export default function CreateAdress() {
-	const [open, setOpen] = useState(false);
-	const dispatch = useDispatch();
-	const [primary, SetPrimary] = useState(false);
-	const user = useSelector((state) => state.user.detail_user.id);
-	const [data, setData] = useState({
-		name: "",
-		phone: "",
-		city: {
-			province_name: "",
-			city: "",
-			sub_district: "",
-			postal_code: [],
-			select: null,
-		},
-		address: "",
-		other: "",
-		name_location: "",
-		user,
-	});
+    const [open, setOpen] = useState(false);
+    const dispatch = useDispatch();
+    const [primary, SetPrimary] = useState(false);
+    const user = useSelector((state) => state.user.detail_user.id);
+    const [data, setData] = useState({
+        name: "",
+        phone: "",
+        city: {
+            province_name: "",
+            city: "",
+            sub_district: "",
+            postal_code: [],
+            select: null,
+        },
+        address: "",
+        other: "",
+        name_location: "",
+        user,
+    });
 
-	const handleChange = (e) => {
-		const { id, value } = e.target;
-		setData((prev) => ({ ...prev, [id]: value }));
-	};
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setData((prev) => ({ ...prev, [id]: value }));
+    };
 
-	const handleOpen = () => setOpen(true);
+    const handleOpen = () => setOpen(true);
 
-	const handleClose = () => setOpen(false);
+    const handleClose = () => setOpen(false);
 
-	const handleCheck = (e) => SetPrimary(e.target.checked);
+    const handleCheck = (e) => SetPrimary(e.target.checked);
 
-	const handleSubmit = () => {
-		let formData = data;
-		formData = {
-			...formData,
-			city: `${formData.city.province_name} ,${formData.city.city} ,${formData.city.sub_district} ,${formData.city.select}`,
-		};
-		axios
-			.post("api/auth/location/", JSON.stringify(formData))
-			.then((res) => {
-				dispatch(appendLocation(res.data));
-				dispatch(sendAlert("success Add Address", 1));
-				if (primary) {
-					dispatch(setPrimaryLocation(res.data.id));
-				}
-				handleClose();
-			})
-			.catch((err) => dispatch(sendAlert(err.request.response, 3)));
-	};
+    const handleSubmit = () => {
+        let formData = data;
+        formData = {
+            ...formData,
+            city: `${formData.city.province_name} ,${formData.city.city} ,${formData.city.sub_district} ,${formData.city.select}`,
+        };
+        axios
+            .post("api/auth/location/", JSON.stringify(formData))
+            .then((res) => {
+                dispatch(appendLocation(res.data));
+                dispatch(sendAlert("success Add Address", 1));
+                if (primary) {
+                    dispatch(setPrimaryLocation(res.data.id));
+                }
+                handleClose();
+            })
+            .catch((err) => dispatch(sendAlert(err.request.response, 3)));
+    };
 
-	return (
-		<div>
-			<button type="button" onClick={handleOpen}>
-				Add New Address
-			</button>
-			<Modal
-				aria-labelledby="transition-modal-title"
-				aria-describedby="transition-modal-description"
-				className={style.modal}
-				open={open}
-				onClose={handleClose}
-				closeAfterTransition
-				BackdropComponent={Backdrop}
-			>
-				<Fade in={open}>
-					<form className={style.paper}>
-						<h2>Add New Address</h2>
+    return (
+        <div>
+            <button type="button" onClick={handleOpen}>
+                Add New Address
+            </button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={style.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+            >
+                <Fade in={open}>
+                    <form className={style.paper}>
+                        <h2>Add New Address</h2>
+                        <FormControl
+                            variant="outlined"
+                            size="small"
+                            style={{ marginBottom: "20px" }}
+                        >
+                            <InputLabel id="name_location">Label Location</InputLabel>
+                            <Select
+                                labelId="name_location"
+                                id="name_location"
+                                value={data.name_location}
+                                onChange={(e) =>
+                                    setData((prev) => ({
+                                        ...prev,
+                                        name_location: e.target.value,
+                                    }))
+                                }
+                            >
+                                <MenuItem id="name_location" value={"rumah"}>
+                                    Rumah
+                                </MenuItem>
+                                <MenuItem id="name_location" value={"kantor"}>
+                                    Kantor
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                        <div className={style.name}>
+                            <TextField
+                                label="Recipient's Name"
+                                style={{ marginRight: "15px", marginBottom: "20px" }}
+                                id="name"
+                                variant="outlined"
+                                size="small"
+                                onChange={(e) => handleChange(e)}
+                            />
+                            <TextField
+                                label="Phone Number"
+                                style={{ marginBottom: "20px" }}
+                                id="phone"
+                                variant="outlined"
+                                size="small"
+                                onChange={(e) => handleChange(e)}
+                            />
+                        </div>
+                        <div className={style.name}>
+                            <AutoAddress
+                                setData={setData}
+                                
+                            />
 
-						<InputLabel id="name_location">Label Location</InputLabel>
-						<Select
-							labelId="name_location"
-							id="name_location"
-							value={data.name_location}
-							onChange={(e) =>
-								setData((prev) => ({
-									...prev,
-									name_location: e.target.value,
-								}))
-							}
-						>
-							<MenuItem id="name_location" value={"rumah"}>
-								Rumah
-							</MenuItem>
-							<MenuItem id="name_location" value={"kantor"}>
-								Kantor
-							</MenuItem>
-						</Select>
-						<div className={style.name}>
-							<TextField
-								label="Recipient's Name"
-								style={{ marginRight: "15px", marginBottom: "20px" }}
-								id="name"
-								variant="outlined"
-								size="small"
-								onChange={(e) => handleChange(e)}
-							/>
-							<TextField
-								label="Phone Number"
-								style={{ marginBottom: "20px" }}
-								id="phone"
-								variant="outlined"
-								size="small"
-								onChange={(e) => handleChange(e)}
-							/>
-						</div>
-						<div className={style.name}>
-							<AutoAddress setData={setData} />
-
-							<AutoPostal
-								options={data.city.postal_code}
-								disable={
-									data.city.postal_code &&
-									data.city.postal_code.length > 0
-								}
-								setData={setData}
-							/>
-						</div>
-						<TextField
-							label="Address"
-							style={{ marginBottom: "20px" }}
-							id="address"
-							variant="outlined"
-							size="small"
-							onChange={(e) => handleChange(e)}
-						/>
-						<TextField
-							style={{ marginBottom: "20px" }}
-							id="other"
-							variant="outlined"
-							size="small"
-							onChange={(e) => handleChange(e)}
-							label="information Address (Optional)"
-						/>
-						{/* yang bawah enak pake opsion kayak rumah / kantor */}
-						<div className={style.check}>
-							<input
-								id="set_primary"
-								type="checkbox"
-								checked={primary}
-								onChange={(e) => handleCheck(e)}
-							/>
-							<label htmlFor="set_primary">set as primary address</label>
-						</div>
-						<div className={style.button}>
-							<Button
-								style={{ marginTop: "15px", width: "150px" }}
-								variant="contained"
-								color="primary"
-								onClick={() => handleSubmit()}
-							>
-								Add
-							</Button>
-							<Button
-								style={{
-									marginTop: "15px",
-									marginLeft: "15px",
-									width: "150px",
-								}}
-								variant="outlined"
-								color="primary"
-								onClick={handleClose}
-							>
-								Cancel
-							</Button>
-						</div>
-					</form>
-				</Fade>
-			</Modal>
-		</div>
-	);
+                            <AutoPostal
+                                options={data.city.postal_code}
+                                disable={data.city.postal_code && data.city.postal_code.length > 0}
+                                setData={setData}
+                            />
+                        </div>
+                        <TextField
+                            label="Address"
+                            style={{ marginBottom: "20px" }}
+                            id="address"
+                            variant="outlined"
+                            size="small"
+                            onChange={(e) => handleChange(e)}
+                        />
+                        <TextField
+                            style={{ marginBottom: "20px" }}
+                            id="other"
+                            variant="outlined"
+                            size="small"
+                            onChange={(e) => handleChange(e)}
+                            label="information Address (Optional)"
+                        />
+                        {/* yang bawah enak pake opsion kayak rumah / kantor */}
+                        <div className={style.check}>
+                            <input
+                                id="set_primary"
+                                type="checkbox"
+                                checked={primary}
+                                onChange={(e) => handleCheck(e)}
+                            />
+                            <label htmlFor="set_primary">set as primary address</label>
+                        </div>
+                        <div className={style.button}>
+                            <Button
+                                style={{ marginTop: "15px", width: "150px" }}
+                                variant="contained"
+                                color="primary"
+                                onClick={() => handleSubmit()}
+                            >
+                                Add
+                            </Button>
+                            <Button
+                                style={{
+                                    marginTop: "15px",
+                                    marginLeft: "15px",
+                                    width: "150px",
+                                }}
+                                variant="outlined"
+                                color="primary"
+                                onClick={handleClose}
+                            >
+                                Cancel
+                            </Button>
+                        </div>
+                    </form>
+                </Fade>
+            </Modal>
+        </div>
+    );
 }
--- components/AuthSystem/Location/createAdress/AutoAddress.js
+++ components/AuthSystem/Location/createAdress/AutoAddress.js
@@ -6,80 +6,73 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import axiosInstance from "../../../../utils/axios";
 
 export default function Grouped({ setData }) {
-	const [open, setOpen] = useState(false);
-	const [options, setOptions] = useState([]);
-	const [value, setValue] = useState(null);
-	const [input, setInput] = useState("");
-	const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [options, setOptions] = useState([]);
+    const [value, setValue] = useState(null);
+    const [input, setInput] = useState("");
+    const [loading, setLoading] = useState(false);
 
-	const getFistlatter = (name) => {
-		const fistlatter = name[0].toUpperCase();
-		return /[0-9]/.test(fistlatter) ? "0-9" : fistlatter;
-	};
-	useEffect(() => {
-		if (input.length > 3) {
-			setLoading(true);
-			axiosInstance
-				.get(`api/auth/location/address/?search=${input}`)
-				.then((res) => {
-					setLoading(false);
-					setOptions(res.data);
-				})
-				.catch((e) =>
-					axios.isCancel(e) ? console.clear() : console.log(e.request)
-				);
-		}
-	}, [input]);
+    const getFistlatter = (name) => {
+        const fistlatter = name[0].toUpperCase();
+        return /[0-9]/.test(fistlatter) ? "0-9" : fistlatter;
+    };
+    useEffect(() => {
+        if (input.length > 3) {
+            setLoading(true);
+            axiosInstance
+                .get(`api/auth/location/address/?search=${input}`)
+                .then((res) => {
+                    setLoading(false);
+                    setOptions(res.data);
+                })
+                .catch((e) => (axios.isCancel(e) ? console.clear() : console.log(e.request)));
+        }
+    }, [input]);
 
-	useEffect(() => {
-		if (!open) setOptions([]);
-	}, [open]);
+    useEffect(() => {
+        if (!open) setOptions([]);
+    }, [open]);
 
-	return (
-		<Autocomplete
-			id="province"
-			style={{ width: 300 }}
-			open={open}
-			value={value}
-			onOpen={() => setOpen(true)}
-			onClose={() => setOpen(false)}
-			onChange={(event, newValue) => {
-				setValue(newValue);
-				setData((prev) => ({
-					...prev,
-					city: { ...newValue, select: null },
-				}));
-			}}
-			inputValue={input}
-			onInputChange={(event, newInputValue) => setInput(newInputValue)}
-			getOptionLabel={(option) =>
-				option
-					? `${option.province_name}, ${option.city}, ${option.sub_district}`
-					: ""
-			}
-			getOptionSelected={(option, value) =>
-				option.province_name === value.province_name
-			}
-			options={options}
-			loading={loading}
-			renderInput={(params) => (
-				<TextField
-					{...params}
-					label="City or Sub District"
-					variant="outlined"
-					InputProps={{
-						...params.InputProps,
-						endAdornment: (
-							<>
-								{loading ? (
-									<CircularProgress color="inherit" size={20} />
-								) : null}
-								{params.InputProps.endAdornment}
-							</>
-						),
-					}}
-				/>
-			)}
-		/>
-	);
+    return (
+        <Autocomplete
+            id="province"
+            style={{ width: 300, marginRight: "15px", marginBottom: "20px" }}
+            open={open}
+            value={value}
+            onOpen={() => setOpen(true)}
+            onClose={() => setOpen(false)}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+                setData((prev) => ({
+                    ...prev,
+                    city: { ...newValue, select: null },
+                }));
+            }}
+            inputValue={input}
+            onInputChange={(event, newInputValue) => setInput(newInputValue)}
+            getOptionLabel={(option) =>
+                option ? `${option.province_name}, ${option.city}, ${option.sub_district}` : ""
+            }
+            getOptionSelected={(option, value) => option.province_name === value.province_name}
+            options={options}
+            loading={loading}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="City or Sub District"
+                    variant="outlined"
+                    size="small"
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <>
+                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                {params.InputProps.endAdornment}
+                            </>
+                        ),
+                    }}
+                />
+            )}
+        />
+    );
 }
